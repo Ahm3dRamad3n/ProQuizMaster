120c52759df798568625ebf85ad1ccc4b1968b75
Ahmed Ramadan Abd Elrady <ahmd.abdalrada2456@compit.aun.edu.eg>
2026-04-21 21:51:01 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,12 @@
 const CACHE_NAME = 'pwa-cache-v1';
 const urlsToCache = [
-  '',
-  'index.html',
-  'manifest.json',
-  'icons/icon-48x48.png',
-  'icons/icon-72x72.png',
-  'icons/icon-96x96.png',
-  'icons/icon-128x128.png',
-  'icons/icon-144x144.png',
-  'icons/icon-152x152.png',
-  'icons/icon-192x192.png',
-  'icons/icon-384x384.png',
-  'icons/icon-512x512.png',
-  'script.js',
-  'style.css',
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png',
+  './script.js',
+  './style.css',
 
 ];
 
